be0dd10903e1e3304e3816ee375b4bd19eaf336d
sachin yadav <yadavsachin3166@gmail.com>
2026-06-20 20:21:35 +0530

    Implemented data model entities

---
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -0,0 +1,86 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue()
+    private long securityId;
+
+    @ManyToOne
+    private Portfolio portfolio;
+
+    private String name;
+    private String category;
+    private double purchasePrice;
+    private LocalDate purchaseDate;
+    private int quantity;
+
+    protected Security() {}
+
+    public Security(Portfolio portfolio, String name, String category,
+                    double purchasePrice, LocalDate purchaseDate,
+                    int quantity) {
+        this.portfolio = portfolio;
+        this.name = name;
+        this.category = category;
+        this.purchasePrice = purchasePrice;
+        this.purchaseDate = purchaseDate;
+        this.quantity = quantity;
+    }
+
+    public long getSecurityId() {
+        return securityId;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
